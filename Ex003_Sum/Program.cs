2a8a7ef06079b49202e382748e70b4f36176dcc7
lordik <jetlord@mail.ru>
2022-09-15 20:26:24 +0300

    Ex003_SumRandom

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 26;
+﻿int numberA = new Random().Next(1, 10);  // ждем случайное число 1,2,3,4,...,9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB; // дополнительный шаг. Можно было пропустить и записать сразу Console.WriteLine(numberA + numberB)
 Console.WriteLine(result);   
