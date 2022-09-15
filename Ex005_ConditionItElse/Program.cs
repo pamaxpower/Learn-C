82923d15c6b58b2fcc4bb90c673352c3e0aae64a
lordik <jetlord@mail.ru>
2022-09-15 20:57:28 +0300

    Ex005_If_else

---
+++ Ex005_ConditionItElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "паша")  //команда ToLower - все символы строки переводит в нижний регистр. Для того чтобы исключить ошибки на вариантах ПАша, ПаШа и тд
+{
+    Console.WriteLine("Привет хозяин! Готов служить тебе");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
