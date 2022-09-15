ff10a192a2ba5d691de2ff7ac673e47514ded286
lordik <jetlord@mail.ru>
2022-09-15 22:43:03 +0300

    Ex007_Cycle

---
+++ Ex007_Cycle/Program.cs
@@ -0,0 +1,48 @@
+﻿
+Console.Clear();            // перед выполнением команды очищает консоль
+
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 25,        // один из вариантов записи переменных, через перечисление
+    xc = 80, yc = 25;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while(count < 1000)
+{
+    int what = new Random().Next(0, 3);      // Определяем интервал. Здесь он [0;3) -> 0,1,2
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);         // На выходе получаем фрактальное изображение треугольника Серпинского
+    Console.WriteLine("+");
+    count++;                                 // Аналогия с count=count+1 и count=+1
+}
