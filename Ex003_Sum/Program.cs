efee879363fab4a8d56307459863d0427b2aa74f
lordik <jetlord@mail.ru>
2022-09-15 20:00:13 +0300

    Ex003

---
+++ Ex003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 31;
+int numberB = 26;
+int result = numberA + numberB; // дополнительный шаг. Можно было пропустить и записать сразу Console.WriteLine(numberA + numberB)
+Console.WriteLine(result);   
