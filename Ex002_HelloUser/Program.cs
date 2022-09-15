f72e0dd1779b2d0ba04f3bafb3e9a3d349a7ce82
lordik <jetlord@mail.ru>
2022-09-15 15:55:51 +0300

    Ex001_01

---
+++ Ex002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя");    //WriteLine - выводит текст на следующую строку, Write - пишет рядом
+string username = Console.ReadLine();    // командой ReadLine считываются данные
+Console.Write("Привет, ");
+Console.Write(username);
