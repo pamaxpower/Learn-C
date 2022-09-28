5d3445ad2808585b932ef043616e3ed8a1c4e16b
lordik <jetlord@mail.ru>
2022-09-28 13:51:23 +0300

    Sem4-1

---
+++ Seminar4/task16/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSum(int num)
+{
+    int sum = 0;
+
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int sum1 = GetSum(num);
+Console.WriteLine($"Результат суммы от 1 до {num} = {sum1}");
---
+++ Seminar4/task17/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void num_count(int num1)
+{
+    int count = 0;
+    while (num1 != 0)
+    {
+        num1 = num1 / 10;                   // работает только с числом до 10 знаков
+        count++;
+    }
+    Console.WriteLine(count);
+}
+
+num_count(num);
